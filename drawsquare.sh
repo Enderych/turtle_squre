568f2294f215cc00f0017716c53cdf346613db71
Administrator <admin@example.com>
2018-08-14 20:19:49 +0000

    Update drawsquare.sh

--- drawsquare.sh
+++ drawsquare.sh
@@ -2,7 +2,7 @@
 set -e
 
 # setup ros environment s
-source "./ros_ws/devel/setup.bash"
+source "/ros_ws/devel/setup.bash"
 #source /opt/ros/kinetic/setup.bash
 
 roslaunch rosdisplay drawsquare.launch
